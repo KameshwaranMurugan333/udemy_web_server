9f413f43faa4531d3579df57796f6e2f29a3e068
KameshwaranMurugan333 <ksmwaran333@gmail.com>
2019-02-12 00:41:06 +0530

    setup heroku port and test script

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 var app = express();
-var port = 3000;
+const port =process.env.PORT || 3000;
 
 hbs.registerPartials(path.join(__dirname,'/views/partials'));
 
 app.set('view-engine','hbs');
-app.use(express.static(path.join(__dirname,'/public')));
 
 hbs.registerHelper('getCurrentYear',()=>{
     return new Date().getFullYear();
@@ -35,9 +34,11 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use((req,res,next)=>{
-    res.render('maintenance.hbs');
-});
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/',(req,res) =>{
     res.render('home.hbs',{
